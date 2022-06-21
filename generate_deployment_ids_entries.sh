8ddc3c9de53fd37fc7e6790ab06e1242b4e70f42
David Lopez <david.lopez@midokura.com>
2022-06-22 00:09:05 +0200

    Get module id from EVP API to generate deployment

--- generate_deployment_ids_entries.sh
+++ generate_deployment_ids_entries.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
-MODULE_ID="PUT HERE A VALID MODULE ID"
+moduleId=$(curl -s http://evp-api.midokura/v2/module \
+  -H "Content-Type: application/json" \
+  -H 'X-Authorization: Bearer '"${TB_TOKEN}" | jq -r .[0].id )
 for i in {1..50}
 do
-	id=$(curl -s  http://evp-api.midokura/v2/deployment \
-	  -H "Content-Type: application/json" \
-    -H 'X-Authorization: Bearer '"${TB_TOKEN}" \
-		-d "{\"specs\":[{\"moduleId\":\"${MODULE_ID}\",\"description\":\"Moduledescription\",\"publishAlias\":[{\"topic\":\"sender-topic\",\"alias\":\"salute-publication\"}],\"subscribeAlias\":[{\"topic\":\"receiver-dlq-topic\",\"alias\":\"errors\"}]},{\"moduleId\":\"${MODULE_ID}\",\"description\":\"Moduledescription\",\"publishAlias\":[{\"topic\":\"sender-dlq-topic\",\"alias\":\"errors\"}],\"subscribeAlias\":[{\"topic\":\"receiver-topic\",\"alias\":\"salute-topic\"}]}],\"connections\":{\"publishTopics\":[],\"subscribeTopics\":[]}}"| jq -r .id)
-	echo "put(${i}, \"${id}\");"
+    id=$(curl -s  http://evp-api.midokura/v2/deployment \
+      -H "Content-Type: application/json" \
+      -H 'X-Authorization: Bearer '"${TB_TOKEN}" \
+      -d "{\"specs\":[{\"moduleId\":\"${moduleId}\",\"description\":\"Moduledescription\",\"publishAlias\":[{\"topic\":\"sender-topic\",\"alias\":\"salute-publication\"}],\"subscribeAlias\":[{\"topic\":\"receiver-dlq-topic\",\"alias\":\"errors\"}]},{\"moduleId\":\"${moduleId}\",\"description\":\"Moduledescription\",\"publishAlias\":[{\"topic\":\"sender-dlq-topic\",\"alias\":\"errors\"}],\"subscribeAlias\":[{\"topic\":\"receiver-topic\",\"alias\":\"salute-topic\"}]}],\"connections\":{\"publishTopics\":[],\"subscribeTopics\":[]}}"| jq -r .id)
+    echo "put(${i}, \"${id}\");"
 done
 
