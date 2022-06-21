dd4df380cec1d582dccc1fc364b17f0742ae835a
David Lopez <david.lopez@midokura.com>
2022-06-22 00:14:09 +0200

    Fix scripts indentation

--- generate_devices_ids_entries.sh
+++ generate_devices_ids_entries.sh
@@ -4,12 +4,12 @@ for i in {1..50}
 do
 	printf -v name 'gatling-test%02d' "$i"
 	iotDeviceId=$(curl -s  http://thingsboard.midokura/api/tenant/devices?deviceName=${name} \
-        -H "X-Authorization: Bearer $TB_TOKEN" \
-	    | jq -r .id.id)
+    -H "X-Authorization: Bearer $TB_TOKEN" \
+    | jq -r .id.id)
 	id=$(curl -s  http://evp-api.midokura/v2/device \
-	  -H "Content-Type: application/json" \
+    -H "Content-Type: application/json" \
     -H 'X-Authorization: Bearer '"${TB_TOKEN}" \
- 		-d "{\"iotDeviceId\": \"${iotDeviceId}\",\"description\": \"Gatling test device: ${name}\"}" \
-  	    | jq -r .id)
+    -d "{\"iotDeviceId\": \"${iotDeviceId}\",\"description\": \"Gatling test device: ${name}\"}" \
+    | jq -r .id)
 	echo "put(${i}, \"${id}\");"
 done
--- get_token.sh
+++ get_token.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 token=$(curl -s  http://thingsboard.midokura/api/auth/login \
-	  -H "Content-Type: application/json" \
-	  -d '{"username":"gomidogo@midokura.com", "password":"GoMidoGo"}'\
-    | jq -r .token)
+  -H "Content-Type: application/json" \
+  -d '{"username":"gomidogo@midokura.com", "password":"GoMidoGo"}'\
+  | jq -r .token)
 echo "Set variable in the environment:"
 echo "export TB_TOKEN=\"${token}\""
